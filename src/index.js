17fb320155aac4212d9355d274cc1c1356f09dfd
Max Rountree <max.r.rountree@gmail.com>
2023-12-22 01:21:02 -0500

    Finished part 3, connected with firebase and redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
+
+// https://react-redux-firebase.com/docs/getting_started.html
+import { Provider } from "react-redux";
+import { createStore, combineReducers } from "redux";
+import {
+  ReactReduxFirebaseProvider,
+  firebaseReducer,
+} from "react-redux-firebase";
+
 import { BrowserRouter } from "react-router-dom";
 import { initializeApp } from 'firebase/app';
+import { composeWithDevTools } from '@redux-devtools/extension';
+
+// https://stackoverflow.com/questions/68946446/how-do-i-fix-a-firebase-9-0-import-error-attempted-import-error-firebase-app
+import firebase from 'firebase/compat/app';
 import firebaseConfig from './firebase.config';
 
 const app = initializeApp(firebaseConfig);
 
+// Add firebase to reducers
+const rootReducer = combineReducers({
+  firebase: firebaseReducer
+  // firestore: firestoreReducer // <- needed if using firestore
+})
+
+// Create store with reducers and initial state
+const store = createStore(rootReducer, composeWithDevTools());
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: 'users'
+  // useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+  // enableClaims: true // Get custom claims along with the profile
+}
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  // createFirestoreInstance // <- needed if using firestore
+};
+
+// Downgrade from React 18 to 17
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
 //   <BrowserRouter>
@@ -16,9 +52,13 @@ const app = initializeApp(firebaseConfig);
 // );
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ReactReduxFirebaseProvider>
+  </Provider>,
   document.getElementById("root")
 );
 
